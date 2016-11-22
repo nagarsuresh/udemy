5ded4c210e5f8ff0827d65535a2af10d61c1af7f
Suresh Nagar <nagar.suresh@gmail.com>
2016-11-21 17:17:44 -0800

    clarity component

--- angular2_max/first-app/src/app/app.module.ts
+++ angular2_max/first-app/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponent } from './app.component';
 import { OtherComponent } from './other/other.component';
 import { AnotherComponent } from './other/another.component';
 import { DatabindingComponent } from './databinding/databinding.component';
+import { ClarityComponent } from './clarity/clarity.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OtherComponent,
     AnotherComponent,
-    DatabindingComponent
+    DatabindingComponent,
+    ClarityComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular2_max/first-app/src/app/clarity/clarity.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fa-clarity',
+  templateUrl: './clarity.component.html',
+  styleUrls: ['./clarity.component.css']
+})
+export class ClarityComponent {
+
+  constructor() { }
+
+}
