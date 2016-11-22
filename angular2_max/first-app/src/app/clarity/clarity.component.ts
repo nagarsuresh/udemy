80c7c7e1dba4fe91d4ac3594b99eac6de01b197e
Suresh Nagar <nagar.suresh@gmail.com>
2016-11-21 18:56:44 -0800

    clarity

--- angular2_max/first-app/src/app/clarity/clarity.component.ts
+++ angular2_max/first-app/src/app/clarity/clarity.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fa-clarity',
   templateUrl: './clarity.component.html',
   styleUrls: ['./clarity.component.css']
 })
-export class ClarityComponent {
+export class ClarityComponent implements OnInit {
 
   constructor() { }
+
+
+  users:any[] = [];
+
+
+  ngOnInit():void{
+    for(let i=0; i<20; i++){
+      this.users.push({
+        id : i,
+        name : 'User-'+i
+      });
+    }
+  }
+
+  onTabSelected(): void{
+    console.log("Tab selected");
+  }
+
+  onTabContentActivated(): void{
+    console.log("onTabContentActivated");
+  }
+
+  onTabIndexChanged(): void{
+    console.log("onTabIndexChanged");
+  }
 
 }
