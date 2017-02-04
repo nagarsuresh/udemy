2e61781ba7b9e89c9e6b3e6a53b9c5c88ad932b9
Suresh Nagar <nagar.suresh@gmail.com>
2017-02-03 22:58:12 -0800

    remove recipe book

--- angular2_max/recipe-book/src/app/recipes/recipe.ts
+++
@@ -1,5 +0,0 @@
-import { Ingredient } from "../shared";
-
-export class Recipe {
-  constructor(public id:number, public name:string, public description:string, public imagePath:string, public ingredients: Ingredient[]) {}
-}
